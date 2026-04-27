e1e6d731381ef7a3ebd7d93926a87632424c3601
Raul Goncalves <raul.oliveira369@gmail.com>
2026-04-27 13:57:47 -0300

    Fix(repo): consertando o repositorio

--- src/modules/auth/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { asyncHandler } from '../../utils/async-handler';
-import { authController } from './auth.controller';
-
-export const authRouter = Router();
-
-authRouter.post('/login', asyncHandler(authController.login));
-authRouter.get('/me', asyncHandler(authController.me));
